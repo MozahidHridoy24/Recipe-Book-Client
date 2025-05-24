97fd016dab6a41d318ae8e9e82992029dffcec21
mozahidhridoy24 <hridoy1407@gmail.com>
2025-05-24 22:36:26 +0600

    changing loading spinner style for darkmode

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="fixed inset-0 flex flex-col justify-center items-center bg-white z-50">
+    <div className="fixed inset-0 flex flex-col justify-center items-center bg-base-100 text-base-content z-50">
       <div className="relative w-16 h-16 mb-4">
         <div className="absolute inset-0 border-4 border-orange-400 rounded-full border-t-transparent animate-spin"></div>
         <div className="absolute inset-2 border-4 border-yellow-300 rounded-full border-b-transparent animate-spin-reverse"></div>
